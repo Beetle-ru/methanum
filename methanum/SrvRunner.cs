b1b42943a89dffed9c9415427c863fe83ac4a700
Beetle_ru <a.zhukov1@tcs5569.tcsbank.ru>
2014-11-27 16:19:11 +0300

    handled exceptions

--- methanum/SrvRunner.cs
+++ methanum/SrvRunner.cs
@@ -8,11 +8,25 @@ using System.Threading.Tasks;
 namespace methanum {
     public class SrvRunner {
         private ServiceHost SHost;
-        public void Start(int port) {
-            var uris = new[] { new Uri(string.Format("net.tcp://localhost:{0}", 2255)) };
 
-            SHost = new ServiceHost(typeof(Gate), uris);
-            SHost.Open();
+        public void Start(int port) {
+            try {
+                var uris = new[] {new Uri(string.Format("net.tcp://localhost:{0}", 2255))};
+
+                SHost = new ServiceHost(typeof (Gate), uris);
+                SHost.Open();
+
+                foreach (Uri uri2 in SHost.BaseAddresses) {
+                    Console.WriteLine("\t{0}", uri2.ToString());
+                }
+                Console.WriteLine("The service is ready.");
+            }
+            catch (TimeoutException timeProblem) {
+                Console.WriteLine(timeProblem.Message);
+            }
+            catch (CommunicationException commProblem) {
+                Console.WriteLine(commProblem.Message);
+            }
         }
 
         public void Stop() {
