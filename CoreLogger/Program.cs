4d3b49e84e4941513a792df1ba68353bef501aaf
Beetle_ru <a.zhukov1@tcs5569.tcsbank.ru>
2015-04-24 14:24:01 +0300

    logger created

---
+++ CoreLogger/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using methanum;
+using System.Threading;
+
+namespace CoreLogger {
+    class Program {
+        public static List<Event> EventPull;
+        public static DateTime Today;
+        public static string FileName;
+        public static StreamWriter FileStreamWriter;
+        public static object SreamLocker;
+        public const int Refrashtimeout = 5000; // ms
+
+        static void Main(string[] args) {
+            args = new[] {"localhost:2255"};
+            if ((!args.Any())) {
+                Console.WriteLine("Usage:");
+                Console.WriteLine("ClentExample.exe coreAddress:port");
+                Environment.Exit(1);
+            }
+
+            EventPull = new List<Event>();
+
+            Today = DateTime.Today;
+
+            SreamLocker = new object();
+
+            FileOpen();
+
+            var lwt = new Thread(LogWriterThread);
+            lwt.IsBackground = true;
+            lwt.Start();
+
+            var maingate = new Connector(args[0]);
+            maingate.ReceiveEvent += AllEvents;
+
+            Connector.HoldProcess();
+        }
+
+        static void AllEvents(Event evt) {
+            
+            lock (EventPull) {
+                if (evt.Destination != "keepAlive") {
+                    EventPull.Add(evt);
+                    Console.Write(".");
+                }
+            }
+        }
+
+        static void LogWriterThread() {
+            var isHas = false;
+
+            while (true) {
+                lock (EventPull) {
+                    isHas = EventPull.Any();
+
+                    if (isHas) {
+                        var evt = EventPull.Last();
+                        EventPull.RemoveAt(EventPull.Count - 1);
+
+                        WriteLog(evt.ToString());
+                        Console.WriteLine(evt.Id);
+                    }
+                }
+
+                if (!isHas) {
+                    Thread.Sleep(Refrashtimeout);
+                    Console.WriteLine("");
+                }
+            }
+            
+        }
+
+        static string GeneratefileName() {
+            FileName = String.Format("log\\{0:0000}{1:00}{2:00}.log", Today.Year, Today.Month, Today.Day);
+            return FileName;
+        }
+
+        static bool DayChanged() {
+            return Today < DateTime.Today;
+        }
+
+        static void FileOpen() {
+            lock (SreamLocker) {
+                Directory.CreateDirectory("log");
+                var filename = GeneratefileName();
+
+                FileStreamWriter = new StreamWriter(filename, true);
+                FileStreamWriter.AutoFlush = true;
+            }
+        }
+
+        static void WriteLog(string message) {
+            lock (FileName) {
+                if (DayChanged() || FileStreamWriter == null) {
+                    FileOpen();
+                    Today = DateTime.Today;
+                }
+            }
+
+            lock (SreamLocker) {
+                FileStreamWriter.WriteLine(message);
+            }
+        }
+    }
+}
---
+++ CoreLogger/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("CoreLogger")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CoreLogger")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("cd2a1acb-e039-4c68-a368-98c0e76f8416")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
